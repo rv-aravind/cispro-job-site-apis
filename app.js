363d67b1d6252850b9eb77b64e8ed4b7a4b25972
rv-aravind <aravindakumar3315@gmail.com>
2025-12-21 22:09:07 +0530

    cors fix for coimbatorejobs domain

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const app = express();
 // Allow requests from specific origins
 const allowedOrigins = [
   'http://localhost:3000',                      // Local frontend
-  'https://coimbatorejobs.in/',         // versal frontend
+  'https://coimbatorejobs.in',         // versal frontend
 ];
 
 app.use(cors({
